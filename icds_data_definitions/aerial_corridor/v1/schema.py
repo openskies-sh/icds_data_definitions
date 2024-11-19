1cdb2f96d6ec10934d236457e3bfdd624c03e207
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2024-11-19 21:26:39 +0000

    Updated schema to move to common and added aerial corridor schema

--- icds_data_definitions/aerial_corridor/v1/schema.py
+++ icds_data_definitions/aerial_corridor/v1/schema.py
@@ -1,26 +1,13 @@
 from pydantic import BaseModel
 from enum import Enum
 
-# Version: 0.0.1
-DATA_DEFINITIONS_VERSION = "0.0.1"
-
-class AvailabilityEnum(str, Enum):
-    unknown = 'unknown'
-    healthy = 'healthy'
-    unhealthy = 'unhealthy'
-
-
-class VersionAvailabilityRead(BaseModel):
-    "A class to hold version and availability of ICDS"
+class AerialCorridorRead(BaseModel):
+    "A class to read data from the Aerial Corridor table "
     
-    version: str
-    availability: AvailabilityEnum
-
-
-
-class VersionAvailabilityWrite(BaseModel):
-    "A class to hold version and availability information to write to the ICDS database, it is normally populated at startup time"
-    id: str
-    version: str
-    availability: AvailabilityEnum
-
+    url: str
+    min_cell_level: int
+    max_cell_level: int
+    start_datetime: str
+    end_datetime: str
+    created_at: str
+    updated_at: str
