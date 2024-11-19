55b8ecf85ccaf8dbb06e02551e7fb0c768d21a43
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2024-11-19 22:24:59 +0000

    Updated version, added aerial corridor write schema

--- icds_data_definitions/aerial_corridor/v1/schema.py
+++ icds_data_definitions/aerial_corridor/v1/schema.py
@@ -10,3 +10,12 @@ class AerialCorridorRead(BaseModel):
     end_datetime: str
     created_at: str
     updated_at: str
+
+class AerialCorridorWrite(BaseModel):
+    "A class to write data to the Aerial Corridor table "
+    id: str
+    url: str
+    min_cell_level: int
+    max_cell_level: int
+    start_datetime: str
+    end_datetime: str
