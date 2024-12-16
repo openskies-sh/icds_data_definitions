9ee312240cadc3679ec776762b9bb688f2014b14
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2024-12-16 21:51:36 +0000

    Fixed import for EntityID

--- icds_data_definitions/aerial_corridor/v1/drone_port_schema.py
+++ icds_data_definitions/aerial_corridor/v1/drone_port_schema.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from enum import Enum
 from typing import Optional
 from uuid import UUID
-from dss import EntityID
+from .dss import EntityID
 from pydantic import BaseModel, Field, confloat, constr
 
 
